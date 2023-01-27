9822a6f3fb521974b3a0f7c75cb0ecaae606b0bb
rondido <rppr001@naver.com>
2023-01-27 15:58:58 +0900

    feat: google login and LoginPage style commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Page/Login";
+import Logo from "../src/comment/Logo";
+import "./Page/style/global.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Login />
     </div>
   );
 }
---
+++ src/Page/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import LogSignButton from '../comment/LogSignButton';
+import './style/LoginPage.scss';
+import './style/global.scss';
+
+export default function Login() {
+  return (
+    <>      
+      <div>
+        <div className='Title'>
+          <h1> 결혼할 사람 </h1>   
+        </div>
+        <div className='container'>
+          <div className='Button'>
+            <LogSignButton/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
