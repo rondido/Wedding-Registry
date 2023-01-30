abe7fccb59e6210ac1f84353253f074efaa47c0a
DESKTOP-12AE4RE\dich2 <dich24@naver.com>
2023-01-30 23:09:05 +0900

    fix: 로고 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Login from "./Page/Login";
 import Signup from "./Page/Signup";
-import Logo from "../src/comment/Logo";
 import "./Page/style/global.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./Page/Main";
 import ProductPage from "./Page/ProductPage";
 import BuyPage from "./Page/BuyPage";
 import Header from "../src/comment/Header";
+import Logo from "./comment/Logo";
 
 function App() {
   return (
