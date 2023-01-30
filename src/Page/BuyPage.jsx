4d410d36b800b0d48eb4b19c2ad0ee4a6409120d
seongj <alfmah2314@gmail.com>
2023-01-30 20:47:50 +0900

    feat: BuyPage 구현

--- src/Page/BuyPage.jsx
+++ src/Page/BuyPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import BuyProduct from "../comment/BuyProduct";
+import "./style/BuyPage.scss";
+import "./style/BuyProduct.scss";
 
 export default function BuyPage() {
     return (
@@ -11,7 +13,11 @@ export default function BuyPage() {
             <div className="logout">
                 <Link to={"/login"} className="logout">logout</Link>
             </div>
+            <p>OOOO님의 리스트 입니다.</p>
             <div className="container">
+                <BuyProduct/>
+                <BuyProduct/>
+                <BuyProduct/>
                 <BuyProduct/>
             </div>
         </>
--- src/comment/BuyProduct.jsx
+++ src/comment/BuyProduct.jsx
@@ -3,7 +3,6 @@ import BuyItem from "./BuyItem";
 export default function BuyProduct() {
     return (
         <div className="productBox">
-            <p>OOOO님의 리스트 입니다.</p>
             <div className="itemList">
                 <BuyItem/>
             </div>
