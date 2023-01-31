3d53056f432c7abd14810ae6152a6e3cef97096d
rondido <rppr001@naver.com>
2023-01-31 15:21:00 +0900

    refac:header or router commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import "./App.css";
 import Login from "./Page/Login";
 import Signup from "./Page/Signup";
-import Logo from "../src/comment/Logo";
 import "./Page/style/global.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./Page/Main";
 import ProductPage from "./Page/ProductPage";
 import Header from "../src/comment/Header";
+import ProductLoginPage from "./Page/ProductLoginPage";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Logo />
+        <Header />
         <Routes>
-          <Route path='/' element={<Main />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/productPage' element={<ProductPage />}></Route>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/product" element={<ProductPage />}></Route>
+          <Route path="/product/login" element={<ProductLoginPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Page/ProductPage.jsx
+++ src/Page/ProductPage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from '../comment/Header'
 import Product from '../comment/Product'
 
-
 export default function ProductPage() {
   return (
     <div>
